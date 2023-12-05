07c0ee993f0a0f00903dcea0868660380447b8cf
Will Leach <leach87@gmail.com>
2023-12-05 18:15:38 -0500

    day 5 works

--- 5/5B - reverse approach.py
+++ 5/5B - reverse approach.py
@@ -1,6 +1,4 @@
 """day 5"""
-_working
-
 maps = ['seed-to-soil map:',
         'soil-to-fertilizer map:',
         'fertilizer-to-water map:',
@@ -8,6 +6,8 @@ maps = ['seed-to-soil map:',
         'light-to-temperature map:',
         'temperature-to-humidity map:',
         'humidity-to-location map:']
+
+reverse_maps = maps[::-1]
 
 #map  soil / destination <- seed / source span
 
@@ -23,30 +23,31 @@ def gen_dicts(a):
             dict_of_maps[dict_map_name] = [] # inititalize the dict
         elif string not in maps and i != 0 and string != '':
             destination, source, span = (int(s) for s in string.split())
-            dict_of_maps[dict_map_name].append((destination, source, span))
+            dict_of_maps[dict_map_name].append((destination, source, span)) 
         elif i == 0 or string == '':
-            print(f"passing or seeds on blanks {i} {string}")
+            pass
+           # print(f"passing or seeds on blanks {i} {string}")
         else: raise Exception(f"{dict_map_name} {destination} {source} {span} | index: {i}")
     return dict_of_maps
 
-def get_next(source, map, dict_of_maps):
-    print(f"source: {source} in map: {map}")
+def get_next(source, map, dict_of_maps): #no change I think
+    #print(f"source: {source} in map: {map}")
     mapping = dict_of_maps[map]
-    print(f'mapping: \n {mapping}')
+    #print(f'mapping: \n {mapping}')
     for i,s in enumerate(mapping):
         dest_beg, source_beg, span = s[0], s[1] , s[2]
         #lot_range = range(source_beg, source_beg + span) numbers wayy too big
-        print(f"Checking if {source} is in {source_beg} - {source_beg + span -1}")
+        #print(f"Checking if {source} is in {source_beg} - {source_beg + span -1}")
         if source_beg <= source <= source_beg + span - 1:
             if dest_beg < source_beg:
                 dif = source_beg - dest_beg
                 destination =  source - dif
-                print (f"location found: {destination} in map: {map}")
+                #print (f"location found: {destination} in map: {map}")
                 return destination
             elif dest_beg > source_beg:
                 dif = dest_beg - source_beg
                 destination = source + dif
-                print (f"location found: {destination} in map: {map}")
+                #print (f"location found: {destination} in map: {map}")
                 return destination
             else: raise Exception(f"this string cause problems {s}")
         elif source_beg > source:
@@ -57,42 +58,112 @@ def get_next(source, map, dict_of_maps):
             pass #not found
     #no mapping
     destination = source
-    print (f"location not found: {destination} equals source {source} in map: {map}")
+    #print (f"location not found: {destination} equals source {source} in map: {map}")
     return destination
 
+def de_overlap_tuples(t):
+    t = sorted(t,key=lambda x: x[0]) #just to be safe
+    no_duplicate_location_tuples = []
+    no_overlaps = True
+    min_x, max_x = (0, 0)
+    for t in t:
+        min_y = t[0]
+        max_y = t[0] + t[1] - 1
+        if max_x >= min_y and max_y > max_x : # 
+            """ ------
+                    -------"""
+            min_y_adjustment = max_x - min_y
+            print(f"Overlap found: {max_x} - {min_y} overlaps by {min_y_adjustment}")
+            max_y = max(max_x,max_y) #probably not needed
+            no_duplicate_location_tuples.append((min_y,max_y))
+            min_x, max_x = min_y, max_y
+
+        elif max_y >= min_x and min_y < min_x:
+            """      ------
+                 -------"""
+            max_y = min_x + 1
+            min_x, max_x = min_y, max_y
+            raise Exception ("This sould not have happened - ovrlap 2. This means something is wrong with sorting")
+        
+        elif max_y > max_x and min_y > max_x:
+            print("The best case no overlap")
+            """ -----
+                       ------   """
+            no_duplicate_location_tuples.append((min_y,max_y))
+            min_x, max_x = min_y, max_y
+
+        elif min_y >= min_y and max_y <= max_x:
+            print(" total overlap. Do nothing")
+            """ -----
+                 --   """
+            min_x, max_x = min_y, max_y
+    print(f"Here is the list of no_duplicate_location_tuples {no_duplicate_location_tuples}")
+    return no_duplicate_location_tuples
+
 def input():
-    with open('5/input.txt') as f:
+    with open('5/test.txt') as f:
         a = f.readlines()
         a = [i.strip() for i in a]
     return a
 
 def main():
+    """Trick is to start from the lowest location and find that seed. Then search if the seed is in there
+    by going through each tuple and perform a z < winning seed < x +range
+
+    o start from bottome of maps, find the destination that is the lowest and journey through that to the 
+    winning seed. I think I will need to refactor some functions."""
+
     a = input()
-    #print(a[:])
     dict_of_maps = gen_dicts(a)
 
     seeds_ = [int(x) for x in a[0].split(":")[1].split() if x.isdigit()]
     seed_tuples =  [(seeds_[i], seeds_[i + 1]) for i in range(0, len(seeds_), 2)]
+    #order does not matter since the mapping journey is random  
+    print(f"These are the seeds that will be checked. Order does not matter since mapping journy is random: {seed_tuples}")
 
-    #Trick is to start from the lowest location and find that seed. Then search if the seed is in there
-    #by going through each tuple and perform a z < winning seed < x +range
+    #locations to check:
+    location_name = reverse_maps[0]
+    print(f"location name: {location_name}")
+    location_tuples = dict_of_maps[location_name]
+    print(f'location tupes presort: {location_tuples}')
+    location_tuples = sorted(location_tuples,key=lambda x: x[0])
+    print(f"These are the locations that will be checked. Here order matters: {location_tuples}")
+    location_tuples = de_overlap_tuples(location_tuples)
+    print(location_tuples)
 
-    ## so start from bottome of maps, find the destination that is the lowest and journey through that to the 
-    #winning seed. I think I will need to refactor some functions.
+    a_ = location_tuples
+    b_ = de_overlap_tuples(a_)
 
-    print(f"seeds: {seeds}")
-    locations = []
-    #map  soil / destination <- seed / source span
-    for seed in seeds:
-        source = seed #        
-        for map in maps:
-           source = get_next(source, map, dict_of_maps) #next source is prior desitation
-        location = source
-        locations.append(location)
+    print("checking if it overlaps works")
+    for x,y in zip(a_,b_):
+        print(f"{x},{y} 0's {abs(x[0]) - abs(y[0])} 1's: {abs(x[0]) - abs(y[0])}")
+        if abs(x[0]) - abs(y[0]) > 10:
+            raise Exception("should not happen")
+        if abs(x[1]) - abs(y[1]) > 10:
+            raise Exception("should not happen")
+
+    max_loc = 0
+
+    for t in location_tuples:
+        candidate_high = t[1]
+        max_loc = max(max_loc,candidate_high)
+    print(f"Checking {max_loc} locations....................................... {max_loc}")
     
-    #do
-    print(f"locations: {locations}")
-    print(f"min location: {min(locations)}")
-
+    for t in range(1,max_loc+1):
+        #each tuple is min and max
+        cycles = 0
+        i=t
+        cycles += 1
+        #i is a location
+        print(f"{i} location checking journey~~~~~{i}")
+        source = i
+        for map in reverse_maps:
+            seed = get_next(source, map, dict_of_maps)
+            source = seed
+        location_seed = seed
+        for t in seed_tuples:
+            if t[0] <= location_seed <= t[1]:
+                print(f"SOLUTION FOUND after {cycles} cycles! It's {i}")
+                return i
 
 main()
---
+++ 5B - reverse approach-simplified.py
@@ -0,0 +1,105 @@
+"""day 5"""
+#works
+maps = ['seed-to-soil map:',
+        'soil-to-fertilizer map:',
+        'fertilizer-to-water map:',
+        'water-to-light map:',
+        'light-to-temperature map:',
+        'temperature-to-humidity map:',
+        'humidity-to-location map:']
+
+reverse_maps = maps[::-1]
+
+#map  soil / destination <- seed / source span
+
+def gen_dicts(a):
+    list_of_indexes = []
+    dict_of_maps = {}
+    for i, string in enumerate(a):
+        if string in maps:
+            dict_ix = i
+            dict_map_name = string #might not do anything with this
+            #dict_map_name, dict_ix,
+            list_of_indexes.append((dict_map_name,dict_ix))
+            dict_of_maps[dict_map_name] = [] # inititalize the dict
+        elif string not in maps and i != 0 and string != '':
+            destination, source, span = (int(s) for s in string.split())
+            dict_of_maps[dict_map_name].append((destination, source, span)) 
+        elif i == 0 or string == '':
+            pass
+           # print(f"passing or seeds on blanks {i} {string}")
+        else: raise Exception(f"{dict_map_name} {destination} {source} {span} | index: {i}")
+    return dict_of_maps
+
+def get_next(dest, map, dict_of_maps): #no change I think
+    mapping = dict_of_maps[map]
+    for s in mapping:
+        b, a, span = s[0], s[1] , s[2]
+        adjustment = abs(b-a) # need to find direction so will play with operator
+        if b>a:
+            adjustment *= -1
+        #print(f"{dest} is between {b} and {b + span}?")       
+        if b <= dest <= b + span -1:
+            a = dest + adjustment
+            #print(f"Yes - completed leg {map} {dest}->{a} -- adjustment {adjustment}")
+            return a
+        else:
+            a = b + adjustment
+    #print(f"No - completed leg of our journey using {map} {dest}->{dest}")  
+    return dest #since if not found they are same
+
+
+def de_overlap_tuples(t):
+    sorted_tuples = sorted(t,key=lambda x: x[0]) #just to be safe
+    res = [sorted_tuples[0]] #initialize 
+    for t in sorted_tuples[1:]:
+        last_t = res[-1]
+        if t[0] > last_t[1]: #what we want
+            res.append(t)
+        else: #overlap
+            res[-1] = (last_t[0],max(last_t[1]),t[1])
+    return res
+            
+def input():
+    with open('5/input.txt') as f:
+        a = f.readlines()
+        a = [i.strip() for i in a]
+    return a
+
+def main():
+    """Trick is to start from the lowest location and find that seed. Then search if the seed is in there
+    by going through each tuple and perform a z < winning seed < x +range
+
+    o start from bottome of maps, find the destination that is the lowest and journey through that to the 
+    winning seed. I think I will need to refactor some functions. Overlaps in seeds """
+
+    a = input()
+    dict_of_maps = gen_dicts(a)
+
+    seeds_ = [int(x) for x in a[0].split(":")[1].split() if x.isdigit()]
+    seed_tuples =  [(seeds_[i], seeds_[i + 1]) for i in range(0, len(seeds_), 2)]
+    seed_tuples_bins = [(x,x+y-1) for x,y in seed_tuples]
+    seed_tuples_bins = de_overlap_tuples(seed_tuples_bins)
+    #order does not matter since the mapping journey is random  
+    #print(f"These are the seeds that will be checked. Order does not matter since mapping journy is random: {seed_tuples_bins}")
+    #locations to check:
+    i = 0
+    flip = True
+    while flip:
+        i += 1
+        #print(f"{i} location checking journey~~~~~~~~~~~~~~~~~~~~~~~{i}")
+        #if i == 36:
+        #    break
+        b = i
+        for map in reverse_maps:
+            b  = get_next(b, map, dict_of_maps)
+        location_seed = b
+        #print(f"{i} location results in seed {location_seed}")
+        for t in seed_tuples_bins:
+            #print(f"Testing loc: {i} which is seed: {location_seed} Is seed between {t[0]} and {t[1]}> ")
+            if t[0] <= location_seed <= t[1]:
+                print(f"YES SOLUTION FOUND after {i} cycles! It's {i}")
+                flip = False
+                return i
+
+main()
