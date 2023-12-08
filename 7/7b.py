6c571ef5baef0d90896a1ccc4af80944f419ca08
Will Leach <leach87@gmail.com>
2023-12-07 20:44:32 -0500

    day 7 works

--- 7/7b.py
+++ 7/7b.py
@@ -1,11 +1,10 @@
 '''--- Day 7: Camel Cards ---'''
 #works
-
 import functools
 import operator
 import collections
 
-labels = ["A", 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+labels = ["A", 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2','J'] #reorder J
 ranks = list(range(len(labels),0,-1))
 labels_rank = dict(zip(labels,ranks))
 
@@ -15,10 +14,33 @@ class Hand:
         self.hand = hand
         self.counts = collections.Counter(hand)
         self.counter_test = self.counts.values()
-        self.hand_strength = self.calc_hand_strengh()
+        self.hand_strength = self.calc_joker_hand_strenghth()
         self.single_cards_strengths = [labels_rank[c] for c in hand ] #5 ranks 13 to 1
+        self.joker_hand = self.hand #initial
+
+    def calc_joker_hand_strenghth(self):
+        j_count = self.counts['J']
+        #print(f"j count: {j_count}, self.counts= {self.counts}")
+        if j_count == 5:
+            return 7
+        elif 1 <= j_count <= 4:
+            temp_counts_not_joker = collections.defaultdict(int)
+            for c in self.hand:
+                if c != 'J':
+                    temp_counts_not_joker[c] += 1
+            top_card = sorted(temp_counts_not_joker.items(),key=lambda x: x[1], reverse=True)[0][0] #first element, and then card
+            self.joker_hand = ''.join([top_card if c == 'J' else c for c in self.hand ]) 
+            #print(f" {self.hand}---->{self.joker_hand}-----------Joker found ---------top card: {top_card}")
+            return self.calc_hand_strengh(J=True)                       
+        else:
+            return self.calc_hand_strengh()
         
-    def calc_hand_strengh(self):
+
+    def calc_hand_strengh(self,J=False):
+        if J == True:
+            og_counts = self.counter_test
+            temp_counts = collections.Counter(self.joker_hand).values()
+            self.counter_test = temp_counts #we replace this back on exit
         strength = 1
         #test for five of a kind:
         if 5 in self.counter_test:
@@ -39,6 +61,9 @@ class Hand:
             strength = 2
         else:
             strength = 1
+
+        if J==True:
+            self.counter_test = og_counts
         return strength
 
 def ranker(hand1,hand2):
@@ -94,5 +119,6 @@ def main(fi):
     print(f"min output: {min_output}, max output: {max_output} range = {max_output - min_output}, test: {min_output<=winnings<=max_output}")
     return winnings
 
-main('7/test.txt')
+#main('7/test.txt')
+#main('7/test_mine.txt')
 main('7/input.txt')
