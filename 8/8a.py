0723ea280484c725c4cfa95af17c44e4219520fc
Will Leach <leach87@gmail.com>
2023-12-08 03:07:15 -0500

    8a

---
+++ 8/8a.py
@@ -0,0 +1,52 @@
+"""--- Day 8: Haunted Wasteland ---"""
+#works
+import re
+
+start = "AAA"
+end = "ZZZ"
+
+class Node():
+    def __init__(self,n,l,r):
+        self.name = n
+        self.left = l
+        self.right = r
+
+def get_input(fi):
+    with open(fi) as f:
+        return f.readlines()
+
+def cycle_1d(s):
+    while True:
+        old = s
+        s =  s[1:] + s[0]
+        yield old[0]
+
+def main(fi):
+    print(f'{fi}')
+    inp = get_input(fi)
+    directions = inp[0].strip()
+    print(directions)
+    nodes = {}
+
+    for i,e in enumerate(inp[2:]):
+        p = re.compile(r'[A-Z]{3}')
+        n,l,r = p.findall(e)
+        nodes[n] = (l,r)
+
+    count = 0
+    next_node = start
+    for d in cycle_1d(directions):
+        count += 1
+        #d is the directions string cycled. We grab the first digit
+        l_r = lambda x: 0 if x == "L" else 1
+        next_node = nodes[next_node][l_r(d)]
+        if next_node == end:
+            break
+        if fi == 'test2.txt' and count == 7:
+            break
+    print(count)
+    return count
+
+main('8/test.txt')
+main('8/test2.txt')
+main('8/input.txt')
