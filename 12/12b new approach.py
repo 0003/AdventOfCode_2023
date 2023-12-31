8232266138ba59e109904c3ae62a64e5a23c0cec
Will Leach <leach87@gmail.com>
2023-12-31 02:52:40 -0500

    day 12b works

--- 12/12b new approach.py
+++ 12/12b new approach.py
@@ -16,16 +16,12 @@ Rule 2 all groups must be valid
 Return sum of all different arrangemetns of all results
 """
 
-#length of numbers is the number of groups
 
-#approach 2 code this out
-
+#works
 import functools
 
-@functools.cache
+@functools.lru_cache
 def solver(record_string, record_counts) -> int:
-    result = 0
-
     #base case from string perspective
     if record_string == "":
         if record_counts == ():
@@ -41,24 +37,26 @@ def solver(record_string, record_counts) -> int:
             return 0 #failed
     
     #non-base case
-    next_char = record_string[0]
-    if next_char in "?.":
+    result = 0
+
+    char = record_string[0]
+    if char in "?.":
+        #advance string one and keep record_counts as is
         result += solver(record_string[1:],record_counts)
     
-    #this would be a new block because above would solve for "."
-    elif next_char in "?#":
+    #this is key before it was an elif, but need to consider that ? is either a "." or a "?"
+    if char in "?#":
         #check count
         next_count = record_counts[0] 
         if next_count <= len(record_string):
             #then there cannot be a "." in the rest of the string
-            if "." not in record_string[:next_count]:
+            if "." not in record_string[:next_count]: # there cannot be a "." because it's cpntiguous
                 # both perspectives
                 if next_count == len(record_string) or record_string[next_count] != "#":
-
-                                                                
-
-
-    
+                    #jumpahead on the ###'s that we know have to exist and move to the next record_counts
+                    result += solver(record_string[next_count + 1:], record_counts[1:])
+        #Here would be elses that I have removed
+    return result
 
 
 def get_input(f):
@@ -66,8 +64,8 @@ def get_input(f):
         rows = []
         for ix in fi:
             _ = ix.split(" ")
-            s = _[0]
-            tuple_of_counts = tuple(int(x) for x in  _[1].strip().split(","))
+            s = "?".join([_[0]] * 5)
+            tuple_of_counts = tuple(int(x) for x in  _[1].strip().split(",")) * 5
             rows.append((s, tuple_of_counts))
     return rows
 
@@ -75,14 +73,17 @@ def main(f):
     rows = get_input(f'12/{f}')
     total_valid_combos = 0
     for i , r in enumerate(rows):
-        print(f"{i}: {r[0]} {r[1]}")
         record_string = r[0]
         record_counts = r[1]
-        total_valid_combos = solver(record_string,record_counts)
+        total_valid_combos += solver(record_string,record_counts)
+        print(f"{i}: {r[0]} {r[1]} {total_valid_combos=}")
 
+    print(f"{total_valid_combos=}")  
     return total_valid_combos
 
 
-main("test.txt")
+#main("test.txt")
+#main("test2.txt")
+#main("test3.txt")
 
-main("input.txt")
+main("input.txt") 
