32aee5a975a0ff509c0ac66ca033b4ff6e684f38
Will Leach <leach87@gmail.com>
2024-12-03 04:04:21 -0500

    wip2

--- 2024/3/3B.py
+++ 2024/3/3B.py
@@ -11,17 +11,36 @@ dont_pattern = r"don't\(\)"
 dont_p = re.compile(dont_pattern)
 
 
-def do_check(lookup_dict,ix):
+def do_check(lookup_dict,ix,dos,donts):
         #check to see if max key is TRUE and that the ix is beyond
     if ix not in lookup_dict.keys():
-        if lookup_dict[max(lookup_dict.keys())] and ix >= max(lookup_dict.keys()):
+        if ix >= dos[-1] > donts[-1]:
             return True
+        else:
+            return False
     else:
         return lookup_dict[ix]
 
 def make_lookup_dict(dos,donts):
-    lookup = {}                    ############### do this
+    dos = [0] + dos
+    lookup = {}                   ############### do this
+    i, j = 0, 0
+    current = None
 
+    while i < len(dos) or j < len(donts):
+          #have a good do  and #donts out      OR    #dont has not come up
+          #should not happen oftent
+        if i < len(dos) and (j >= len(donts) or dos[i] <= donts[j]):
+            current = dos[i]
+            i += 1
+        #main driver go until you hit a dont
+        elif j < len(donts):
+            if current is not None:
+                for x in range(current,donts[j] + 1):
+                    lookup[x] = True
+                current = None
+            j += 1
+    return lookup
 
 def get_input(f):
     with open(f"2024/3/{f}") as fi:
@@ -33,22 +52,24 @@ def main(f):
     s = ''.join(li)
 
     sums_of_muls = 0
-    switch = True
 
     muls = list(p.finditer(s))
     dos = list(i.span()[0] for i in do_p.finditer(s))
     donts = list(i.span()[0] for i in dont_p.finditer(s))
+    lookup = make_lookup_dict(dos,donts)
 
     for mul in muls:
         print(mul)
-        if do_check(dos,donts,mul.span()[0],switch):
+        if do_check(lookup,mul.span()[0],dos,donts):
             m = int(mul.group(1)) * int(mul.group(2))
             sums_of_muls += m
             print(f"{mul.group(0)} = {m} : {sums_of_muls}")
 
+    print(sums_of_muls)
     return sums_of_muls
 
-main('test.txt')
+main('test1.txt')
+#main('test2.txt')
 #main('input.txt')
 
 """
