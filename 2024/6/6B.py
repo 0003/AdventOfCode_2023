a56c7cba5cc7c4149594fe4984b5f6b465065865
Will Leach <leach87@gmail.com>
2024-12-07 14:39:20 -0500

    Day 7 par 1 works

---
+++ 2024/6/6B.py
@@ -0,0 +1,62 @@
+#Use the current route bc you can only make 1 edit
+#test on what causes a loop. Traverse problem already solved last year.
+
+import string
+
+def print_map(li):
+    for i in li:
+        print(i)
+
+def get_input(f):
+    with open(f"2024/6/{f}") as fi:
+        map = ["0" + x.strip() + "0" for x in fi]
+        padding = "0" * len(map[0])
+        map = [padding] + map + [padding]
+        return map
+
+def main(f):
+    map = get_input(f)
+    print_map(map)
+
+    path = []
+    travel_map = [c for c in map]
+
+    for i, row in enumerate(map):
+        for j, char in enumerate(row):
+            if char == "^":
+                cursor = (i,j)
+                direction = (-1,0) #north
+                path.append(cursor)
+
+    exit = False
+    while not exit:
+        i, j = cursor[0], cursor[1]
+        i_prime, j_prime = i + direction[0], j + direction[1]
+        next_loc = map[i_prime][j_prime]
+
+        if next_loc == "0":
+            exit = True
+            travel_map[i] = travel_map[i][:j] + "!" + travel_map[i][j+1:]
+            print_map(travel_map)
+            print(f"{len(path) = }")  
+
+        elif next_loc == "#":
+            if direction == (-1,0):
+                direction = (0,1) 
+            elif direction == (0,1):
+                direction = (1,0) 
+            elif direction == (1,0):
+                direction = (0,-1) 
+            elif direction == (0,-1):
+                direction = (-1,0) 
+        else:
+            if next_loc == "." or next_loc == "^":
+                cursor = (i_prime, j_prime)
+                if cursor not in path:
+                    path.append(cursor)
+                travel_map[i] = travel_map[i][:j] + string.ascii_uppercase[(len(path) - 1 )% len(string.ascii_uppercase)] + travel_map[i][j+1:]
+
+    return path
+
+#main('test.txt')
+main("input.txt")
---
+++ 2024/7/7A.py
@@ -0,0 +1,49 @@
+#works
+from itertools import product
+
+operators = ("+","*")
+
+def gen_combos(operators : tuple, operator_spaces_count : int):
+    for combo in product(operators,repeat=operator_spaces_count):
+        yield combo
+
+def test_equation(test_value,numbers,operator_combo):
+    eqaution_result = numbers[0]
+    for i, n in enumerate(numbers[1:]):
+        operator = operator_combo[i]
+        if operator == "+":
+            eqaution_result += n
+        elif operator == "*":
+            eqaution_result *= n
+    return test_value == eqaution_result
+
+
+def get_input(f):
+    with open(f"2024/7/{f}") as fi:
+        calibrations = []
+        for e in  fi:
+            #190: 10 19
+            e_ = e.split(":")
+            test_value = int(e_[0])
+            numbers = [int(x) for x in e_[1].strip().split(" ")]
+            calibrations.append( (test_value,numbers))
+    
+    return calibrations
+
+def main(f):
+    calibrations = get_input(f)
+    print(calibrations)
+
+    score = 0
+    for e in calibrations:
+        test_value = e[0]
+        numbers = e[1]
+        operator_spaces_count = len(numbers) - 1
+        for operator_combo in  gen_combos(operators, operator_spaces_count):
+            if test_equation(test_value,numbers,operator_combo):
+                score += test_value
+                break
+    print (f"Final Score = {score}")
+
+#main("test.txt")
+main("input.txt")
